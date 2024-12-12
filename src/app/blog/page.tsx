977868a5f9d58627ce1c9399ae49cba6e4609f93
Khuldoon171 <kzclever41@gmail.com>
2024-12-12 13:38:07 +0500

    add page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import Group7B from "../../../public/my stuff/Group7B.png";
+import Group185 from "../../../public/my stuff/Group185.png";
+import Image from "next/image";
+import Frame161 from "../../../public/my stuff/Frame161.png";
+import Footer from "@/components/footer";
 
 const Blog = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="w-full h-auto">
+      {/* Blog Image */}
+      <div className="w-full h-[316px] sm:h-[400px] md:h-[500px]">
+        <Image
+          className="w-full h-full object-cover"
+          src={Group7B}
+          alt="Group7B-image"
+          width={2000}
+          height={2000}
+        />
+      </div>
 
-export default Blog
+      {/* Main Image Section */}
+      <div className="w-full h-auto my-10">
+        <Image
+          className="w-full h-auto object-contain"
+          src={Group185}
+          alt="Group-image"
+          width={2000}
+          height={2000}
+        />
+      </div>
+
+      {/* Bottom Image Section */}
+      <div className="w-full h-[270px] sm:h-[300px] md:h-[400px] mt-20">
+        <Image
+          className="w-full h-full object-cover"
+          src={Frame161}
+          alt="Frame-161"
+          width={2000}
+          height={2000}
+        />
+      </div>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
+
+export default Blog;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Contact1 from '../../../public/my stuff/Contact1.png'
+import Image from 'next/image'
+import FormC from '../../../public/my stuff/FormC.png'
+import Footer from '@/components/footer'
+import Frame161 from "../../../public/my stuff/Frame161.png";
+const Contact = () => {
+  return (
+    <div className='w-ful h-auto'>
+      {/* Contact image */}
+      <div className="w-full h-[316px] sm:h-[400px] md:h-[500px]">
+        <Image className="w-full h-full object-cover"
+          src={Contact1}
+          alt="Group7B-image"
+          width={2000}
+          height={2000}
+        />
+      </div>
+      {/* Center image */}
+      <p className='w-full h-auto my-10'>
+        <Image src={FormC} alt="alt" width={2000} height={2000} />
+      </p>
+       {/* Bottom Image Section */}
+       <div className="w-full h-[270px] sm:h-[300px] md:h-[400px] mt-20">
+        <Image
+          className="w-full h-full object-cover"
+          src={Frame161}
+          alt="Frame-161"
+          width={2000}
+          height={2000}
+        />
+      </div>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  )
+}
+
+export default Contact
